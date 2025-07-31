dae706454dea5e975fd63a49a512295442a450e3
wpetto <whdossantos2809@gmail.com>
2025-07-31 15:00:30 -0300

    adicionando novos testes executados baseados na planilha de elaboração dos casos de teste da funcionalidade 'Busca de Filmes' da US001

--- cypress/e2e/busca.cy.js
+++ cypress/e2e/busca.cy.js
@@ -1,30 +1,77 @@
 /// <reference types="cypress" />
 
-describe('US-00-Funcionalidade: busca de filmes', () => {
+describe('US001-Funcionalidade: Busca de filmes', () => {
   beforeEach(() => {
-     cy.visit('/')
+    cy.visit('/')
   });
 
-  it.skip('Deve buscar filmes com sucesso', () => {
+  //CT001 
+  /*
+  it('Busca de filmes com palavra-chave válida', () => {
     cy.get('#search-input').type('marvel')
-    cy.contains('#search-button','Buscar').click()
-    cy.get('#results-section').should('contain' , 'Marvel')
+    cy.contains('#search-button', 'Buscar').click()
+
+    cy.get('#results-section').should('contain', 'Marvel')
   })
 
-  it('Deve buscar filmes com sucesso de uma lista', () => {
+  it('Busca de filmes com palavra-chave válida de uma lista', () => {
     cy.fixture('filmes').then((filmes) => {
-    cy.get('#search-input').type(filmes[0].titulo)
-    cy.contains('#search-button','Buscar').click()
-    cy.get('#results-section').should('contain', filmes[0].titulo)
+      cy.get('#search-input').type(filmes[0].titulo)
+      cy.contains('#search-button', 'Buscar').click()
+
+      cy.get('#results-section').should('contain', filmes[0].titulo)
+    })
   })
+  */
+ 
+  //CT001 
+  it('Busca todos os filmes com palavra-chave válida de uma lista', () => {
+    cy.fixture('filmes').each((filmes) => {
+      cy.get('#search-input').clear().type(filmes.titulo)
+      cy.contains('#search-button', 'Buscar').click()
+
+      cy.get('#results-section').should('contain', filmes.titulo)
+    })
   })
 
-  it('Deve buscar filmes com sucesso de uma lista', () => {
-    cy.fixture('filmes').each((filmes) => {
-    cy.get('#search-input').clear().type(filmes.titulo)
-    cy.contains('#search-button','Buscar').click()
-    cy.get('#results-section').should('contain', filmes.titulo)
+  //CT002
+  it('Busca de filmes sem palavra-chave válida', () => {
+    cy.get('#search-input')
+    cy.contains('#search-button', 'Buscar').click()
+    cy.on('window:alert', (mensagem) => {  //Pega a alert e verifica se o texto está incluso
+      expect(mensagem).to.include('Por favor, digite o nome de um filme')
+    })
   })
+
+  //CT003
+  it('Busca de filmes com palavra-chave válida com espaço', () => {
+    cy.get('#search-input').type('marvel ')
+    cy.contains('#search-button', 'Buscar').click()
+
+    cy.get('#results-section').should('contain', 'Marvel')
+    //Deve aparecer resultados mesmo com espaço após o texto
   })
-    
+
+  //CT004
+  it('Busca de filmes sem resultados', () => {
+    cy.get('#search-input').type('ma')
+    cy.contains('#search-button', 'Buscar').click()
+
+    cy.get('#results-section').should('contain', 'Filme não encontrado.')
+    //Não deve aparecer resultado
+  })
+
+  //CT005
+  it('Limpeza da Busca de Filmes', () => {
+    cy.fixture('filmes').then((filmes) => {
+      cy.get('#search-input').type(filmes[2].titulo)
+      cy.contains('#search-button', 'Buscar').click()
+
+      cy.get('#results-section').should('contain', filmes[2].titulo)
+
+      cy.contains('#clear-button', 'Limpar Busca').click()
+      cy.get('#results-section').should('contain', '')
+    })
+  })
+
 })
