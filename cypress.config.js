93809c52629a22c3cd90280cf8d1a8c98f434257
wpetto <whdossantos2809@gmail.com>
2025-08-08 17:34:41 -0300

    Adiciona vídeo da execução do teste

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
-    baseUrl: "http://127.0.0.1:8080/"
+    baseUrl: "http://127.0.0.1:8080/",
+    video: true
   },
 });
